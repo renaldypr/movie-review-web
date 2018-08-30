4a37847a30d4433d3b88d932dcd6396a1a033865
Renaldy Pratama <renaldy.pratama19@gmail.com>
2018-08-30 23:45:45 +0700

    progress

--- models/user.js
+++ models/user.js
@@ -1,11 +1,45 @@
 'use strict';
+
+//const seque
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     gender: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email format!'
+        },
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    }
+  });
+
+  // Instance Method
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password)
+  }
+
   User.associate = function(models) {
     User.belongsToMany(models.Movie, {through: models.Review, foreignKey: 'userId'})
     User.hasMany(models.Vote, {foreignKey: 'userId'})
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controller/userController');
 
-router.get('/', (req,res) => {
-  
-})
+// router.get('/', (req,res) => {
+//   res.render('add', {errors: req.session.erros});
+//   req.session.errors = null;
+// })
 
 router.get('/register', UserController.showAll)
 router.post('/register', UserController.register)
+
+router.get('/login', (req,res) => {
+  res.render('login')
+})
 
 module.exports = router;
